bb91c589ef7283d43f5b69870365704ffb6d8104
Florent BOURASSE <fbourasse@jahia.com>
2019-10-20 18:59:25 +0200

    Remove of incorrect licenses and author mentions

--- src/main/java/org/jahia/modules/saml2/admin/SAML2SettingsFilter.java
+++ src/main/java/org/jahia/modules/saml2/admin/SAML2SettingsFilter.java
@@ -1,26 +1,3 @@
-/**
- * ==========================================================================================
- * =                            JAHIA'S ENTERPRISE DISTRIBUTION                             =
- * ==========================================================================================
- *
- *                                  http://www.jahia.com
- *
- * JAHIA'S ENTERPRISE DISTRIBUTIONS LICENSING - IMPORTANT INFORMATION
- * ==========================================================================================
- *
- *     Copyright (C) 2002-2016 Jahia Solutions Group. All rights reserved.
- *
- *     This file is part of a Jahia's Enterprise Distribution.
- *
- *     Jahia's Enterprise Distributions must be used in accordance with the terms
- *     contained in the Jahia Solutions Group Terms & Conditions as well as
- *     the Jahia Sustainable Enterprise License (JSEL).
- *
- *     For questions regarding licensing, support, production usage...
- *     please contact our team at sales@jahia.com or go to http://www.jahia.com/license.
- *
- * ==========================================================================================
- */
 package org.jahia.modules.saml2.admin;
 
 import org.jahia.services.render.RenderContext;
@@ -30,9 +7,6 @@ import org.jahia.services.render.filter.RenderChain;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * @author kevan
- */
 public class SAML2SettingsFilter extends AbstractFilter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SAML2SettingsFilter.class);
--- src/main/java/org/jahia/modules/saml2/admin/SAML2SettingsListener.java
+++ src/main/java/org/jahia/modules/saml2/admin/SAML2SettingsListener.java
@@ -1,25 +1,3 @@
-/**
- * ==========================================================================================
- * =                            JAHIA'S ENTERPRISE DISTRIBUTION                             =
- * ==========================================================================================
- * <p>
- * http://www.jahia.com
- * <p>
- * JAHIA'S ENTERPRISE DISTRIBUTIONS LICENSING - IMPORTANT INFORMATION
- * ==========================================================================================
- * <p>
- * Copyright (C) 2002-2016 Jahia Solutions Group. All rights reserved.
- * <p>
- * This file is part of a Jahia's Enterprise Distribution.
- * <p>
- * Jahia's Enterprise Distributions must be used in accordance with the terms contained in the Jahia Solutions Group
- * Terms & Conditions as well as the Jahia Sustainable Enterprise License (JSEL).
- * <p>
- * For questions regarding licensing, support, production usage... please contact our team at sales@jahia.com or go to
- * http://www.jahia.com/license.
- * <p>
- * ==========================================================================================
- */
 package org.jahia.modules.saml2.admin;
 
 import javax.jcr.observation.Event;
